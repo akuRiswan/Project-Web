84baa5f14c6f3d67592dcf3c2e022de2609e9a0f
rrsa <70390184+poxucers@users.noreply.github.com>
2023-05-23 13:46:37 +0800

    Add files via upload

---
+++ alumni/assets/js/script.js
@@ -0,0 +1,52 @@
+(function($) { 
+	$(function() { 
+  
+	  //  open and close nav 
+	  $('#navbar-toggle').click(function() {
+		$('nav ul').slideToggle();
+	  });
+  
+  
+	  // Hamburger toggle
+	  $('#navbar-toggle').on('click', function() {
+		this.classList.toggle('active');
+	  });
+  
+  
+	  // If a link has a dropdown, add sub menu toggle.
+	  $('nav ul li a:not(:only-child)').click(function(e) {
+		$(this).siblings('.navbar-dropdown').slideToggle("slow");
+  
+		// Close dropdown when select another dropdown
+		$('.navbar-dropdown').not($(this).siblings()).hide("slow");
+		e.stopPropagation();
+	  });
+  
+  
+	  // Click outside the dropdown will remove the dropdown class
+	  $('html').click(function() {
+		$('.navbar-dropdown').hide();
+	  });
+	}); 
+	})(jQuery); 
+	
+	document.addEventListener("DOMContentLoaded", function() {
+		const scrollButton = document.getElementById("scrollButton");
+		const section1 = document.getElementById("section");
+		const abou = document.getElementById("section");
+		const zoomInText = document.querySelector(".zoom-in-text");
+	
+		scrollButton.addEventListener("click", function(event) {
+			event.preventDefault();
+			section1.scrollIntoView({ behavior: "smooth" });
+			zoomInText.classList.remove("hidden");
+		});
+	
+		const nextButton = document.querySelector(".next-button");
+		const sliderContent = document.querySelector(".slider-content");
+	
+		nextButton.addEventListener("click", function() {
+			sliderContent.scrollLeft += 300;
+		});
+	});
+	
